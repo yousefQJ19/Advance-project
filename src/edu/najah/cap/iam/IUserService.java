2d7d68aed6828d29ebc86aaa154c88af0796a1c4
ammar shtayeh <76966536+ammarshtayeh@users.noreply.github.com>
2023-12-26 01:55:10 +0200

    Delete src/edu/najah/cap directory

--- src/edu/najah/cap/iam/IUserService.java
+++
@@ -1,8 +0,0 @@
-package edu.najah.cap.iam;
-
-public interface IUserService {
-    void addUser(UserProfile user);
-    void updateUser(UserProfile user);
-    void deleteUser(String userName);
-    UserProfile getUser(String userName);
-}
